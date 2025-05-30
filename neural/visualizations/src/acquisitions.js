f4b596396e074eccec67df74c084ee7d284f6d6b
UltimateCoderDelta <josephyvar@gmail.com>
2025-05-30 23:23:39 +0200

    add all files from original project 1

---
+++ neural/visualizations/src/acquisitions.js
@@ -0,0 +1,50 @@
+import { getAquisitionsByYear } from './api';
+import {
+  Chart,
+  Colors,
+  BarController,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Legend
+} from 'chart.js'
+
+Chart.register(
+  Colors,
+  BarController,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Legend
+);
+
+(async function() {
+  const data = await getAquisitionsByYear();
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      options: {
+        animation: false,
+        plugins: {
+            legend: {
+                display:false
+            },
+            tooltip: {
+                enabled: false
+            }
+        }
+      },
+      data: {
+        labels: data.map(row => row.year),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
