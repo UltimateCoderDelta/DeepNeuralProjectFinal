80cdec50ad19e7177d853d4b8e7231b573fa8a4e
UltimateCoderDelta <josephyvar@gmail.com>
2025-05-31 12:38:54 +0200

    perform database migrate to fix fetch issue

--- neural/ai_models.py
+++ neural/ai_models.py
@@ -3,15 +3,19 @@ import os
 import time
 
 # Use a pipeline as a high-level helper
-from transformers import pipeline
+# from transformers import pipeline
 
-pipe = pipeline("summarization", model="Falconsai/text_summarization")
+# pipe = pipeline("summarization", model="Falconsai/text_summarization")
 
-def generate_text(input_text, model=pipe, max_length=600, min_length=80, print_time_taken=False):
-  start = time.time()
-  output = model(input_text, max_length=max_length, min_length=min_length)
-  end = time.time()
-  print(f"Total Time Elapsed: {end - start:.2f}s")
-  return ("SUMMARY: \n" + output[0]['summary_text'])
+# def generate_text(input_text, model=pipe, max_length=600, min_length=80, print_time_taken=False):
+#   start = time.time()
+#   output = model(input_text, max_length=max_length, min_length=min_length)
+#   end = time.time()
+#   print(f"Total Time Elapsed: {end - start:.2f}s")
+#   return ("SUMMARY: \n" + output[0]['summary_text'])
+
+
+def generate_text(text):
+  return text
 
 
